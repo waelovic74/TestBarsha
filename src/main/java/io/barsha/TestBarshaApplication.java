63a9e192bda8c327b60d5ce40e3003c08c7e5c43
waelovic74 <waelbenmloud.tuto@gmail.com>
2020-12-19 14:59:02 +0100

    US-001: Initialisation de projet Back

---
+++ src/main/java/io/barsha/TestBarshaApplication.java
@@ -0,0 +1,13 @@
+package io.barsha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBarshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBarshaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/barsha/TestBarshaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.barsha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBarshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
